8989f0ea092e0b8dc1c66c671f39bd041e2d10b7
Michelle Lo <michellelo@live.ca>
2024-09-05 18:37:43 -0400

    got api working

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`
+        );
+        console.log(response.data);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchWeather();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       Set Up
