a83287ff5eac9bb26cd23cecabd0d28ddba39008
Michelle Lo <michellelo@live.ca>
2024-09-05 19:44:27 -0400

    review data - possibly make it dynmanic to the last 5 days from current day

--- app/components/WeatherHistory.tsx
+++ app/components/WeatherHistory.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-type Props = {};
+type Props = { date: string; maxTemp: number; minTemp: number };
 
 export default function WeatherHistory({}: Props) {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const fetchWeatherHistory = async () => {
       try {
@@ -11,6 +13,12 @@ export default function WeatherHistory({}: Props) {
           `https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=43.7001&longitude=-79.4163&start_date=2024-09-01&end_date=2024-09-05&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=America%2FNew_York`
         );
         console.log(response.data);
+        const formattedData = response.data.daily.time.map((date, index) => ({
+          date,
+          maxTemp: response.data.daily.temperature_2m_max[index],
+          minTemp: response.data.daily.temperature_2m_min[index],
+        }));
+        setData(formattedData);
       } catch (error) {
         console.error(error);
       }
@@ -18,5 +26,21 @@ export default function WeatherHistory({}: Props) {
     fetchWeatherHistory();
   }, []);
 
-  return <div>WeatherHistory</div>;
+  return (
+    <div>
+      <h1>WeatherHistory</h1>
+      {data.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        data.map((weather, index) => (
+          <div key={index}>
+            <p>Date: {weather.date}</p>
+            <p>Max Temp: {weather.maxTemp}°C</p>
+            <p>Min Temp: {weather.minTemp}°C</p>
+            <hr />
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
